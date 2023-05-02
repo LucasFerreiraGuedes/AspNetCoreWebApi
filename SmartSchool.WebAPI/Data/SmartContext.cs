b58a3be8761e2b7ec673ec276816b2415a85794c
LucasFerreiraGuedes <lucasguedes01673874@gmail.com>
2023-05-02 15:24:18 -0300

    Adicionando a primeira Migration

---
+++ SmartSchool.WebAPI/Data/SmartContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmartSchool.WebAPI.Data
+{
+	public class SmartContext : DbContext
+	{
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+
+        public DbSet<Professor> Professores { get; set; }
+
+        public DbSet<AlunoDisciplina> AlunosDisciplinas { get; set; }
+
+
+        public SmartContext(DbContextOptions<SmartContext> options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.Entity<AlunoDisciplina>()
+				.HasKey(AD => new { AD.AlunoId, AD.DisciplinaId });
+			
+		}
+
+
+	}
+}
